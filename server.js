d7b57d912071854034a0e4996d6deedbad5413a0
Milton <coral.milton@tutanota.com>
2022-01-19 16:06:26 -0500

    Subida Codigo lectura de tablas

--- server.js
+++ server.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const app = express();
 const hbs = require('hbs');
+var bodyParser = require('body-parser')
 
 require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+const { Pool } = require('pg')
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'uni_relacional',
+    password: '1234',
+    port: 5432,
+})
+
+app.get('/list', function(request, response) {
+    console.log('GET request received at /')
+    pool.query('select *   from cabecera limit 1', function(err, result) { // consulta de base de datos a postgres
+        if (err) throw err;
+        else {
+            response.send(result.rows[0]) // se manda el JSON
+        }
+
+    });
+});
+
+
+
 
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs'); //rendiriza la vista 
